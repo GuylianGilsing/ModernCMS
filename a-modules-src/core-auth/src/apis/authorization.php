47a1bf0919cfac1a3d0e87bcca60136e70c2f1bc
Gilsing,Guylian Gilsing G <g.gilsing@student.fontys.nl>
2022-11-05 15:51:56 +0100

    Changed location of authorization based logic to authorization API

--- a-modules-src/core-auth/src/apis/authorization.php
+++ a-modules-src/core-auth/src/apis/authorization.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace ModernCMS\Module\CoreAuth\APIs\Authorization;
 
+use ErrorException;
+use ModernCMS\Module\CoreAuth\Abstractions\Users\User;
+
+use function ModernCMS\Core\APIs\Database\get_database_connection_instance;
 use function ModernCMS\Core\APIs\Hooks\Filters\dispatch_filter;
+use function ModernCMS\Module\CoreAuth\APIs\Users\get_user_by_id;
 
 /**
  * Retrieves all registered roles.
@@ -38,4 +43,109 @@ function get_registered_permissions(): array
     }
 
     return $permissions;
+}
+
+/**
+ * @param int $id The user ID.
+ *
+ * @return array<string>
+ */
+function get_user_permissions(int $id): array
+{
+    $connection = get_database_connection_instance();
+    $queryBuilder = $connection->createQueryBuilder();
+
+    $result = $queryBuilder->select('permission')
+                            ->from('user_permissions')
+                            ->where('user_id = :id')
+                            ->setParameter('id', $id)
+                            ->executeQuery();
+
+    $permissions = [];
+
+    foreach ($result->fetchAllAssociative() as $column)
+    {
+        $permissions[] = $column['permission'];
+    }
+
+    return $permissions;
+}
+
+function _set_stored_permissions_on_user(User $user): void
+{
+    $permissions = get_user_permissions($user->getId());
+    $user->setPermissions($permissions);
+}
+
+/**
+ * Attempts to update the permissions of an existing user.
+ *
+ * @param User $user the user abstraction that you want to update.
+ *
+ * @throws ErrorException when a user abstraction is given that does not exist.
+ *
+ * @return bool Returns true if the update succeeded, false otherwise.
+ */
+function update_user_permissions(User $user): bool
+{
+    _delete_non_selected_user_permissions($user->getId(), $user->getPermissions());
+
+    if (get_user_by_id($user->getId()) === null)
+    {
+        throw new ErrorException("User with ID \"{$user->getId()}\" does not exist.");
+    }
+
+    $existingPermissions = get_user_permissions($user->getId());
+
+    $connection = get_database_connection_instance();
+
+    foreach ($user->getPermissions() as $permission)
+    {
+        if (!in_array($permission, $existingPermissions))
+        {
+            $queryBuilder = $connection->createQueryBuilder();
+            $inserted = $queryBuilder->insert('user_permissions')
+                                        ->values([
+                                            'user_id' => ':id',
+                                            'permission' => ':permission'
+                                        ])
+                                        ->where('user_id = :id')
+                                        ->setParameter('id', $user->getId())
+                                        ->setParameter('permission', $permission)
+                                        ->executeStatement();
+
+            if ($inserted === 0)
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function _delete_non_selected_user_permissions(int $userId, array $permissions): void
+{
+    $connection = get_database_connection_instance();
+    $queryBuilder = $connection->createQueryBuilder();
+
+    if (count($permissions) === 0)
+    {
+        $queryBuilder->delete('user_permissions')
+                    ->where('user_id = :id')
+                    ->setParameter('id', $userId)
+                    ->executeStatement();
+        return;
+    }
+
+    $queryBuilder->delete('user_permissions')
+                    ->where(
+                        $queryBuilder->expr()->and(
+                            $queryBuilder->expr()->eq('user_id', ':id'),
+                            $queryBuilder->expr()->notIn('permission', '(:permissions)')
+                        )
+                    )
+                    ->setParameter('id', $userId)
+                    ->setParameter('permissions', $permissions)
+                    ->executeStatement();
 }
--- a-modules-src/core-auth/src/apis/users.php
+++ a-modules-src/core-auth/src/apis/users.php
@@ -14,6 +14,8 @@ use function ModernCMS\Core\APIs\Mapping\get_array_mapper_instance;
 use function ModernCMS\Module\CoreAuth\APIs\Authentication\get_authentication_token_jwt;
 use function ModernCMS\Module\CoreAuth\APIs\Authentication\is_logged_in;
 use function ModernCMS\Module\CoreAuth\APIs\Authentication\JWT\parse_jwt_token_string;
+use function ModernCMS\Module\CoreAuth\APIs\Authorization\_set_stored_permissions_on_user;
+use function ModernCMS\Module\CoreAuth\APIs\Authorization\update_user_permissions;
 
 function get_user_by_id(int $id): ?User
 {
@@ -274,109 +276,4 @@ function update_user(User $user): bool
     }
 
     return true;
-}
-
-/**
- * @param int $id The user ID.
- *
- * @return array<string>
- */
-function get_user_permissions(int $id): array
-{
-    $connection = get_database_connection_instance();
-    $queryBuilder = $connection->createQueryBuilder();
-
-    $result = $queryBuilder->select('permission')
-                            ->from('user_permissions')
-                            ->where('user_id = :id')
-                            ->setParameter('id', $id)
-                            ->executeQuery();
-
-    $permissions = [];
-
-    foreach ($result->fetchAllAssociative() as $column)
-    {
-        $permissions[] = $column['permission'];
-    }
-
-    return $permissions;
-}
-
-function _set_stored_permissions_on_user(User $user): void
-{
-    $permissions = get_user_permissions($user->getId());
-    $user->setPermissions($permissions);
-}
-
-/**
- * Attempts to update the permissions of an existing user.
- *
- * @param User $user the user abstraction that you want to update.
- *
- * @throws ErrorException when a user abstraction is given that does not exist.
- *
- * @return bool Returns true if the update succeeded, false otherwise.
- */
-function update_user_permissions(User $user): bool
-{
-    _delete_non_selected_user_permissions($user->getId(), $user->getPermissions());
-
-    if (get_user_by_id($user->getId()) === null)
-    {
-        throw new ErrorException("User with ID \"{$user->getId()}\" does not exist.");
-    }
-
-    $existingPermissions = get_user_permissions($user->getId());
-
-    $connection = get_database_connection_instance();
-
-    foreach ($user->getPermissions() as $permission)
-    {
-        if (!in_array($permission, $existingPermissions))
-        {
-            $queryBuilder = $connection->createQueryBuilder();
-            $inserted = $queryBuilder->insert('user_permissions')
-                                        ->values([
-                                            'user_id' => ':id',
-                                            'permission' => ':permission'
-                                        ])
-                                        ->where('user_id = :id')
-                                        ->setParameter('id', $user->getId())
-                                        ->setParameter('permission', $permission)
-                                        ->executeStatement();
-
-            if ($inserted === 0)
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-function _delete_non_selected_user_permissions(int $userId, array $permissions): void
-{
-    $connection = get_database_connection_instance();
-    $queryBuilder = $connection->createQueryBuilder();
-
-    if (count($permissions) === 0)
-    {
-        $queryBuilder->delete('user_permissions')
-                    ->where('user_id = :id')
-                    ->setParameter('id', $userId)
-                    ->executeStatement();
-        return;
-    }
-
-    $queryBuilder->delete('user_permissions')
-                    ->where(
-                        $queryBuilder->expr()->and(
-                            $queryBuilder->expr()->eq('user_id', ':id'),
-                            $queryBuilder->expr()->notIn('permission', '(:permissions)')
-                        )
-                    )
-                    ->setParameter('id', $userId)
-                    ->setParameter('permissions', $permissions)
-                    ->executeStatement();
 }
